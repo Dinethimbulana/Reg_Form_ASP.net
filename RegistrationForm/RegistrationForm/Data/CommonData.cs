9039e12631f1603fc41be17a7381c7d8e56ea377
Dinethimbulana <53506617+Dinethimbulana@users.noreply.github.com>
2022-03-23 14:52:57 +0530

    Add files via upload

---
+++ RegistrationForm/RegistrationForm/Data/CommonData.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ASharp.BasicLayered.Data
+{
+    public class CommonData
+    {
+        public static SqlConnection Connection { get; private set; }
+
+        /// <summary>
+        ///     Prepare the database connection
+        /// </summary>
+        public static void ReadyConnection()
+        {
+            Connection = new SqlConnection();
+            Connection.ConnectionString =
+                ConfigurationManager.ConnectionStrings["BasicLayeredConnection"].ToString();    ///////////////
+            Connection.ConnectionString =
+                @"Data Source=DESKTOP-B0R6CS7\SQLEXPRESS;Initial Catalog=BasicLayeredDB;Integrated Security=True";///////////////////
+            if (Connection.State != ConnectionState.Open) Connection.Open();
+        }
+
+        /// <summary>
+        ///     To execute non query data.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public static int ExecuteNonQuery(SqlCommand command)
+        {
+            try
+            {
+                ReadyConnection();
+                command.Connection = Connection;
+                var result = command.ExecuteNonQuery();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                command.Dispose();
+                if (Connection.State == ConnectionState.Open) Connection.Close();
+                Connection.Dispose();
+            }
+        }
+
+        /// <summary>
+        ///     To get tabular data.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public static SqlDataReader ExecuteReader(SqlCommand command)
+        {
+            try
+            {
+                ReadyConnection();
+                command.Connection = Connection;
+                return command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static object ExecuteScalar(SqlCommand command)
+        {
+            try
+            {
+                ReadyConnection();
+                command.Connection = Connection;
+
+                var value = command.ExecuteScalar();
+                if (value is DBNull)
+                    return default(decimal);
+                return value;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        
+            }
+        }
+
+
+        
+       
+
+
+        
+        
+
+     
+    
---
+++ RegistrationForm/RegistrationForm/Data/DetailsData.cs
@@ -0,0 +1,37 @@
+﻿using ASharp.BasicLayered.Data;
+using RegistrationForm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Data
+{
+    public class DetailsData
+    {
+        public void Insert(detailsDomain domain)
+        {
+
+            var command = new SqlCommand("InsertData"); //name of sp
+            CommonData.ExecuteNonQuery(Constructcommand(command, domain));
+        }
+
+        public SqlCommand Constructcommand(SqlCommand command, detailsDomain domain)
+        {
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@Fname", domain.FName);
+            command.Parameters.AddWithValue("@Lname", domain.LName);
+            command.Parameters.AddWithValue("@Email", domain.Email);
+            command.Parameters.AddWithValue("@Gender", domain.Gender);
+            command.Parameters.AddWithValue("@Address", domain.Address);
+            command.Parameters.AddWithValue("@PhoneNo", domain.PNo);
+            command.Parameters.AddWithValue("@Password", domain.Password);
+
+            return command;
+
+        }
+    }
+}
